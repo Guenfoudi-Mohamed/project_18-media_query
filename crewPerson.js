2546407b4cf04766a108df6fa106a9669e70de52
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-06-13 17:54:16 +0100

    Update crewPerson.js

--- crewPerson.js
+++ crewPerson.js
@@ -3,7 +3,7 @@
 const circles = document.querySelectorAll('body #main #crew .container .content .contentLeft .circles .circle');
 for(let i = 0;i<circles.length;i++){
     circles[i].addEventListener('click',function(){
-        const arrPerson = ['/images/starter-code/assets/crew/image-douglas-hurley.png','/images/starter-code/assets/crew/image-anousheh-ansari.png','/images/starter-code/assets/crew/image-mark-shuttleworth.png','/images/starter-code/assets/crew/image-victor-glover.png'];
+        const arrPerson = ['images/starter-code/assets/crew/image-douglas-hurley.png','images/starter-code/assets/crew/image-anousheh-ansari.png','images/starter-code/assets/crew/image-mark-shuttleworth.png','/images/starter-code/assets/crew/image-victor-glover.png'];
         const persons = document.querySelectorAll('body #main #crew .container .content .contentLeft .crewPerson .person');
         const imgPerson = document.querySelector('body #main #crew .container .content .contentRight img'); 
         for(let x = 0;x<persons.length;x++){
